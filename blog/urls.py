c8b302e0ad8919d324f556b9bd613d4bf48dd4c2
JavGarb <olguiyjavi@gmail.com>
2023-04-09 02:19:40 -0500

    Agregando posts desde front- Aprendiendo django

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blogListView
+from .views import blogListView, BlogCreateView
 
 app_name="blog"
 
 urlpatterns = [
-    path('', blogListView.as_view(), name='home'),    
+    path('', blogListView.as_view(), name='home'),
+    path('create/', BlogCreateView.as_view(), name='create'),    
 ]
